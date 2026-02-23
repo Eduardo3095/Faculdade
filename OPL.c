2699d8f512c40f339916461ef2541df27012d921
Eduardo3095 <dubreezy41@gmail.com>
2026-02-23 16:48:27 -0300

    Add OPL.c to check if numbers are positive

---
+++ OPL.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int mani(){
+
+    int a = 10, b = 5;
+
+    if (a > 0 && b > 0){
+        printf("Os dois numeros sao positivos\n");
+    }
+    
+    else {
+        printf("Um dos numeros eh negativo\n");
+    }
+
+
+    return 0;
+}
